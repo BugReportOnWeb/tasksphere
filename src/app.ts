448a48d3cfbad7f3ca57b80c326d5186ef0c6803
Dev <flashytesting@gmail.com>
2023-11-21 19:33:31 +0530

    Add request logging middleware

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import { taskRoutes } from './routes/taskRoutes';
+import logging from './middleware/logging';
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 const app = express();
 
+// Middlewares
 app.use(express.json());
+app.use(logging);
+
+// Routes
 app.use('/tasks', taskRoutes);
 
 app.listen(PORT, () => {
