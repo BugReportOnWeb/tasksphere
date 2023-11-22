29b990bfeabb36cf715c769f367928415f3299fa
Dev <flashytesting@gmail.com>
2023-11-23 01:35:32 +0530

    Add validation rules for user route request

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -6,4 +6,13 @@ const taskValidationRules = [
     body('completed').isBoolean().withMessage('Completed must be a boolean'),
 ]
 
-export default taskValidationRules;
+const userValidationRules = [
+    body('email')
+        .notEmpty().withMessage('Email is required')
+        .isEmail().withMessage('Invalid email'),
+    body('password')
+        .notEmpty().withMessage('Password is required')
+        .isStrongPassword().withMessage('Password not strong enough')
+]
+
+export { taskValidationRules, userValidationRules };
