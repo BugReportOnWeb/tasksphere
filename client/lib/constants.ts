9a3fa52c8e385a7d94ded2a346164ac92bc5a876
Dev <flashytesting@gmail.com>
2023-11-29 00:14:11 +0530

    Add sample (testing) task data

---
+++ client/lib/constants.ts
@@ -0,0 +1,9 @@
+import { UpdateTaskReq }  from '../types/Task';
+
+const sampleTask: UpdateTaskReq = {
+    title: "Random title",
+    description: "Some random descriptiong for random title (testing purpose)",
+    completed: false
+}
+
+export { sampleTask };
