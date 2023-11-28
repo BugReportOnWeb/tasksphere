147aefb4deaa3d96dc1b9e842d20a6361587da94
Dev <flashytesting@gmail.com>
2023-11-28 20:01:42 +0530

    Refactor components and root file

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,120 +1,35 @@
 'use client'
 
-import TaskCard from "@/components/TaskCard";
-import { Task } from "@/types/Task";
-import { getTasks, removeTask, updateTask } from "@/lib/tasks";
 import { useEffect, useState } from "react";
+import { TaskContext } from "@/context/tasksContext";
+import {  Task } from "@/types/Task";
+import { getTasks } from "@/lib/tasks";
+import Tasks from "@/components/Tasks";
 
 const App = () => {
     const currentUser = 'Dev';
     const [tasks, setTasks] = useState<Task[] | null>(null);
 
-    // TODO: CHange to some util using context for tasks and setter function
-    const handleRemoveTask = async (taskId: string) => {
-        // Optimistic UI
-        // TODO: Do with state for actual Optimistic UI
-        setTasks(prevTasks => {
-            if (prevTasks) {
-                return prevTasks.filter(task => task.id !== taskId);
-            } else {
-                return prevTasks;
-            }
-        })
-
-        // Actual action
-        const removedTask = await removeTask(taskId);
-        console.log(removedTask);
-    }
-
-    const handleUpdateTask = async (taskId: string) => {
-        // // Optimistic UI
-        // // TODO: Do with state for actual Optimistic UI
-        // // CHECK: Use of not null operator
-        let newUpdatedTask = tasks!.find(task => task.id === taskId);
-        if (newUpdatedTask) newUpdatedTask = { ...newUpdatedTask, completed: !newUpdatedTask.completed };
-        const filteredTasks = tasks!.filter(task => task.id !== taskId);
-
-        setTasks(prevTasks => {
-            if (prevTasks) {
-                if (newUpdatedTask) {
-                    return [...filteredTasks, newUpdatedTask]
-                } else { return prevTasks;
-                }
-            } else {
-                return prevTasks;
-            }
-        })
-
-        // Actual action
-        if (newUpdatedTask) {
-            const completedTask = await updateTask(newUpdatedTask);
-            console.log(completedTask);
-        }
-    }
-
     useEffect(() => {
-        const fetchData = async () => {
-            const tempTasks = await getTasks();
-            if (tempTasks) setTasks(tempTasks);
+        const fetchTask = async () => {
+            const fetchedTasks = await getTasks();
+            setTasks(fetchedTasks);
         }
 
-        fetchData();
+        fetchTask();
     }, [])
 
-
-
     return (
-        <div className='my-40'>
-            <h1 className='text-5xl font-extrabold mb-10'>{currentUser}'s Data</h1>
-            {/* CHECK: Using 'not null operator' intead of '&&' */}
-            <div className='flex justify-between gap-10'>
-                <div className='flex flex-col gap-5 w-full'>
-                    <div className='flex items-center gap-3'>
-                        <h1 className='font-extralight text-sm'>Uncompleted Tasks</h1>
-                        <div className='w-10 border border-gray-500'></div>
-                    </div>
-
-                    {
-                        // Check array of uncompleted tasks
-                        tasks
-                            ?.filter(task => !task.completed)
-                            .length === 0 && <h1 className='text-[#a1a1aa]'>No tasks!</h1>
-                    }
-
-                    {tasks?.map(task => (
-                        !task.completed && <TaskCard
-                            updateTask={() => handleUpdateTask(task.id)}
-                            removeTask={() => handleRemoveTask(task.id)}
-                            key={task.id}
-                            task={task}
-                        />
-                    ))}
-                </div>
-                <div className='flex flex-col gap-5 w-full'>
-                    <div className='flex items-center gap-3'>
-                        <h1 className='font-extralight text-sm'>Completed Tasks</h1>
-                        <div className='w-10 border border-gray-500'></div>
-                    </div>
-
-                    {
-                        // Check array of completed tasks
-                        tasks
-                            ?.filter(task => task.completed)
-                            .length === 0 && (<h1 className='text-[#a1a1aa]'>No tasks!</h1>)
-                    }
-
-                    {tasks?.map(task => (
-                        task.completed && <TaskCard
-                            updateTask={() => handleUpdateTask(task.id)}
-                            removeTask={() => handleRemoveTask(task.id)}
-                            key={task.id}
-                            task={task}
-                        />
-                    ))}
-
+        <TaskContext.Provider value={{ tasks, setTasks }}>
+            <div className='my-40'>
+                <h1 className='text-5xl font-extrabold mb-10'>{currentUser}'s Data</h1>
+                {/* CHECK: Using 'not null operator' intead of '&&' */}
+                <div className='flex justify-between gap-10'>
+                    <Tasks completed={false} />
+                    <Tasks completed={true} />
                 </div>
             </div>
-        </div>
+        </TaskContext.Provider>
     )
 }
 
--- client/components/TaskCard.tsx
+++ client/components/TaskCard.tsx
@@ -1,13 +1,50 @@
-import { Task } from "@/types/Task"
+import { useContext } from "react";
+import { TaskContext } from "@/context/tasksContext";
+import { Task, TaskContextType } from "@/types/Task"
+import { removeTask, updateTask } from "@/lib/tasks";
 import Button from "./Button";
 
 interface TaskCardProps {
     task: Task;
-    updateTask: () => void;
-    removeTask: () => void;
 }
 
-const TaskCard = ({ task, updateTask, removeTask }: TaskCardProps) => {
+const TaskCard = ({ task }: TaskCardProps) => {
+    const { tasks, setTasks } = useContext(TaskContext) as TaskContextType;
+
+    const handleRemoveTask = async (taskId: string) => {
+        // Optimistic UI
+        setTasks(prevTasks => {
+            return prevTasks!.filter(task => task.id !== taskId);
+        })
+
+        // Actual action
+        const removedTask = await removeTask(taskId);
+        console.log(removedTask);
+    }
+
+    const handleUpdateTask = async (taskId: string) => {
+        // // Optimistic UI
+        setTasks(prevTasks => {
+            return prevTasks!.map(
+                task => task.id === taskId
+                    ? { ...task, completed: !task.completed }
+                    : task
+            )
+        })
+
+        // Actual action
+        let newUpdatedTask = tasks!.find(task => task.id === taskId);
+        if (newUpdatedTask) newUpdatedTask = {
+            ...newUpdatedTask,
+            completed: !newUpdatedTask.completed
+        };
+
+        if (newUpdatedTask) {
+            const completedTask = await updateTask(newUpdatedTask);
+            console.log(completedTask);
+        }
+    }
+
     return (
         <div className='flex justify-between items-start border border-[#27272a] p-6 rounded-md'>
             <div className='flex flex-col space-y-1.5'>
@@ -18,9 +55,9 @@ const TaskCard = ({ task, updateTask, removeTask }: TaskCardProps) => {
                 </h1>
             </div>
             <div className='flex flex-col gap-3'>
-                {!task.completed && <Button onClick={updateTask} color="green">Done {'->'}</Button>}
-                {task.completed && <Button onClick={updateTask} color="blue">{'<-'} Revert</Button>}
-                <Button onClick={removeTask} color="red">Delete</Button>
+                {!task.completed && <Button onClick={() => handleUpdateTask(task.id)} color="green">Done {'->'}</Button>}
+                {task.completed && <Button onClick={() => handleUpdateTask(task.id)} color="blue">{'<-'} Revert</Button>}
+                <Button onClick={() => handleRemoveTask(task.id)} color="red">Delete</Button>
             </div>
         </div>
     )
