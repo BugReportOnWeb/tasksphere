81523856c6292993cc963f23816f264abd27acae
Dev <flashytesting@gmail.com>
2023-11-29 19:05:14 +0530

    Add additional __v and _id field to Task type

--- client/types/Task.ts
+++ client/types/Task.ts
@@ -1,14 +1,15 @@
 import { Dispatch, SetStateAction } from "react"
 
 interface Task {
-    id: string
-    title: string
-    description: string
-    completed: boolean
-    userEmail: string
+    __v: number;
+    _id: string;
+    title: string;
+    description: string;
+    completed: boolean;
+    userEmail: string;
 }
 
-interface UpdateTaskReq extends Omit<Task , "id" | "userEmail"> {}
+interface UpdateTaskReq extends Omit<Task , "__v" | "_id" | "userEmail"> {}
 
 interface TaskContextType {
     tasks: Task[] | null;
