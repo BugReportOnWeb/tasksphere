29d7cde1017b954fdbc02ea326adae20c217f6e8
Dev <flashytesting@gmail.com>
2023-11-29 22:40:31 +0530

    Add auth/task context provider for routes

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,12 @@
-import type { Metadata } from 'next'
+'use client'
+
+// import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { useState } from 'react';
+import { Task } from '@/types/Task';
+import { AuthContext } from '@/context/AuthContext';
+import { TaskContext } from '@/context/TaskContext';
 
 interface RootLayoutProps {
     children: React.ReactNode;
@@ -8,20 +14,27 @@ interface RootLayoutProps {
 
 const inter = Inter({ subsets: ['latin'] })
 
-const metadata: Metadata = {
-    title: 'Task Manager',
-    description: 'A Task Manager to manage tasks obv ;)',
-}
+// const metadata: Metadata = {
+//     title: 'Task Manager',
+//     description: 'A Task Manager to manage tasks obv ;)',
+// }
 
 const RootLayout = ({ children }: RootLayoutProps) => {
+    const [tasks, setTasks] = useState<Task[] | null>(null);
+    const [currentAuthUser, setCurrentAuthUser] = useState<string | null>(null);
+
     return (
         <html lang="en">
             <body className={`${inter.className} antialiased max-w-5xl mx-auto leading-7`}>
-                {children}
+                <AuthContext.Provider value={{ currentAuthUser, setCurrentAuthUser }}>
+                    <TaskContext.Provider value={{ tasks, setTasks }}>
+                        {children}
+                    </TaskContext.Provider>
+                </AuthContext.Provider>
             </body>
         </html>
     )
 }
 
-export { metadata };
+// export { metadata };
 export default RootLayout;
