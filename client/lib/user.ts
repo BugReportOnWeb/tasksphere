5c2ec534227d86a3bab16b9c8233c8a4a2815cc1
Dev <flashytesting@gmail.com>
2023-12-02 22:53:30 +0530

    Create utils lib for user specific API actions

---
+++ client/lib/user.ts
@@ -0,0 +1,34 @@
+const token = process.env.NEXT_PUBLIC_TEMP_TOKEN as string;
+
+interface UserDetails {
+    username: string,
+    email: string,
+    password: string
+}
+
+const register = async (userDetails: UserDetails) => {
+    const { email, password } = userDetails;
+    const newUserDetails = { email, password };
+
+    try {
+        const res = await fetch('http://localhost:4000/api/users/register', {
+            method: 'POST',
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newUserDetails),
+            cache: 'no-store'
+        })
+
+        const data = await res.json();
+        if (!res.ok) throw data.error;
+
+        return data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+export { register };
