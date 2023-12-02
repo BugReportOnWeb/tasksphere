b0c65485de130a4ff8aca01003fbd9081b48f976
Dev <flashytesting@gmail.com>
2023-12-03 01:01:48 +0530

    Create button component for auth form status

---
+++ client/components/AuthButton.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { useFormStatus } from "react-dom";
+
+const AuthButton = () => {
+    const { pending } = useFormStatus()
+
+    return (
+        <button
+            aria-disabled={pending}
+            className={`${pending ? 'cursor-wait' : 'cursor-pointer'} inline-flex w-full whitespace-nowrap items-center justify-center px-4 py-2 border border-[#27272a] font-medium text-sm rounded-md transition-colors cursor-pointer bg-white text-[#27272a] hover:bg-white/90`}
+        >
+            {pending ? 'Loading...' : 'Register'}
+        </button>
+    )
+}
+
+export default AuthButton;
