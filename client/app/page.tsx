69833193ee84380cfe9e1e8166cea15f5c987ffa
Dev <flashytesting@gmail.com>
2023-12-04 00:04:47 +0530

    Use authorized user context for data fetching

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -7,12 +7,16 @@ import { sampleTask } from "@/lib/constants";
 import { addTask, getTasks } from "@/lib/tasks";
 import Button from "@/components/Button";
 import Tasks from "@/components/Tasks";
+import { redirect } from "next/navigation";
+import { AuthContext } from "@/context/AuthContext";
+import { AuthContextType, AuthUser } from "@/types/auth";
 
 const App = () => {
     // TODO: Change to dynamic when using login res from localStorage
-    const currentUser = 'Dev';
-    const currentUserEmail = 'testing@testing.com';
+    // const [currentUser, setCurrentUser] = useState<User | null>(null);
+    // const currentUserEmail = 'testing@testing.com';
 
+    const { currentAuthUser, setCurrentAuthUser } = useContext(AuthContext) as AuthContextType;
     const { setTasks } = useContext(TaskContext) as TaskContextType;
 
     const handleAddTask = async () => {
@@ -23,38 +27,55 @@ const App = () => {
                 ...sampleTask,
                 __v: 0,
                 _id: randomId,
-                userEmail: currentUserEmail
+                // CHECK: User of 'not null' operator here
+                userEmail: currentAuthUser?.email!
             }];
         })
 
         // Actual action
-        const addedTask = await addTask(sampleTask);
-        console.log(addedTask);
+        if (currentAuthUser) {
+            const addedTask = await addTask(
+                sampleTask,
+                currentAuthUser?.token
+            );
 
-        // Change in from optimistic tasks
-        // Update to new server received value (id, userEmail)
-        setTasks(prevTasks => (
-            prevTasks!.map(task => (
-                task._id === randomId
-                    ? addedTask
-                    : task
+            console.log(addedTask);
+
+            // Change in from optimistic tasks
+            // Update to new server received value (id, userEmail)
+            setTasks(prevTasks => (
+                prevTasks!.map(task => (
+                    task._id === randomId
+                        ? addedTask
+                        : task
+                ))
             ))
-        ))
+        }
     }
 
     useEffect(() => {
-        const fetchTask = async () => {
-            const fetchedTasks = await getTasks();
+        const fetchTask = async (currentUser: AuthUser) => {
+            const fetchedTasks = await getTasks(currentUser.token);
             setTasks(fetchedTasks);
         }
 
-        fetchTask();
+        if (localStorage.getItem('user')) {
+            // TODO: Fix all these naming confusion
+            // Between 'currentUser' and 'currentAuthUser'
+            const currentUser: AuthUser = JSON.parse(localStorage.getItem('user')!);
+
+            setCurrentAuthUser(currentUser);
+            fetchTask(currentUser);
+        } else {
+            redirect('/login');
+        }
+
     }, [])
 
     return (
         <div className='my-40'>
             <div className='flex justify-between items-center mb-10'>
-                <h1 className='text-5xl font-extrabold'>{currentUser}'s Data</h1>
+                <h1 className='text-5xl font-extrabold'>{currentAuthUser?.email}'s Data</h1>
                 <Button onClick={handleAddTask} className='bg-white text-[#27272a] hover:bg-white/90'>Add Task</Button>
             </div>
             <div className='flex justify-between gap-10'>
