42ae515eff2e46eca2b646e9c1685a9c7433885a
Dev <flashytesting@gmail.com>
2023-12-04 17:36:00 +0530

    Add type prop for login and register pages

--- client/components/AuthButton.tsx
+++ client/components/AuthButton.tsx
@@ -2,7 +2,11 @@
 
 import { useFormStatus } from "react-dom";
 
-const AuthButton = () => {
+interface AuthButtonProp {
+    type: string;
+}
+
+const AuthButton = ({ type }: AuthButtonProp) => {
     const { pending } = useFormStatus()
 
     return (
@@ -10,7 +14,7 @@ const AuthButton = () => {
             aria-disabled={pending}
             className={`${pending ? 'cursor-wait' : 'cursor-pointer'} inline-flex w-full whitespace-nowrap items-center justify-center px-4 py-2 border border-[#27272a] font-medium text-sm rounded-md transition-colors cursor-pointer bg-white text-[#27272a] hover:bg-white/90`}
         >
-            {pending ? 'Loading...' : 'Register'}
+            {pending ? 'Loading...' : type === 'register' ? 'Register' : 'Login'}
         </button>
     )
 }
