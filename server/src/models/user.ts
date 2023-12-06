b97fb4815059f64147b0d207e1f5d0e779437ea0
Dev <flashytesting@gmail.com>
2023-12-06 17:18:25 +0530

    Add username field to user model

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -3,12 +3,14 @@ const { Schema } = mongoose;
 
 // Type for Typescript
 interface UserType {
+    username: string;
     email: string;
     password: string;
 }
 
 // Type/Schema for MongodDB
 const UserSchema = new Schema({
+    username: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true }
 })
