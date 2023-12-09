05ac9d3edb14c852df7b5781c8259cf5dd75d459
Dev <flashytesting@gmail.com>
2023-12-09 18:53:44 +0530

    Add sendRequest util for auth request (unused)

--- client/lib/user.ts
+++ client/lib/user.ts
@@ -11,7 +11,35 @@ const BASE_URL = process.env.NODE_ENV === 'development'
     ? process.env.NEXT_PUBLIC_DEV_URL
     : process.env.NEXT_PUBLIC_PROD_URL
 
-// TODO?: Maybe DRY in here...
+// TOOD: Refactor for multiple errors and use when ready
+// Move to utils part (maybe?)
+const sendRequest = async (
+    userDetails: LoginUserDetails | RegisterUserDetails,
+    slug: string
+) => {
+    try {
+        const res = await fetch(`${BASE_URL}/api/users/${slug}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(userDetails),
+            cache: 'no-store'
+        })
+
+        const data = await res.json();
+
+        if (res.status === 400) {
+            throw new Error(data.errors[0].msg);
+        }
+
+        if (!res.ok) throw new Error(data.error);
+
+        return data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+// TODO?: Maybe DRY in here... -> (sendRequest)
 const register = async (userDetails: RegisterUserDetails) => {
     try {
         const res = await fetch(`${BASE_URL}/api/users/register`, {
