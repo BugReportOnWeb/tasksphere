b4011017081e30e7df63ad599e8252be95125737
Dev <flashytesting@gmail.com>
2023-12-11 20:54:12 +0530

    Create sendAuthRequest helper function for auth actions

--- client/app/(auth)/actions.ts
+++ client/app/(auth)/actions.ts
@@ -1,50 +1,21 @@
 'use server'
 
 import { login, register } from "@/lib/user";
-import { AuthUser } from "@/types/auth";
+import { AuthUser, AuthUserCheck } from "@/types/auth";
 
-const registerUser = async (_prevState: any, formData: FormData) => {
-    // CHECK: Valid use of 'as' in this
-    const username = formData.get('username') as string;
-    const email = formData.get('email') as string;
-    const password = formData.get('password') as string;
-
-    const userDetails = { username, email, password };
-
+// Here 'Type' : LoginUserDetails | RegisterUserDetails
+const sendAuthRequest = async <T>(
+    userDetails: T,
+    authRequest: (userDetails: T) => Promise<any>
+) => {
     try {
         // CHECK?: Type checking could be done on the first server request
         // check on @/lib/user
-        const user: AuthUser = await register(userDetails);
-        const newState = {
+        const user: AuthUser = await authRequest(userDetails);
+        const newState: AuthUserCheck = {
             user,
             error: null
         };
-
-        return newState;
-    } catch (error) {
-        if (error instanceof Error) {
-            const newState = {
-                user: null,
-                error: error.message
-            };
-
-            return newState;
-        }
-    }
-}
-
-const loginUser = async(_prevState: any, formData: FormData) => {
-    const email = formData.get('email') as string;
-    const password = formData.get('password') as string;
-
-    const userDetails = { email, password };
-
-    try {
-        const user: AuthUser = await login(userDetails);
-        const newState = {
-            user, 
-            error: null
-        }
 
         return newState;
     } catch (error) {
@@ -57,6 +28,28 @@ const loginUser = async(_prevState: any, formData: FormData) => {
             return newState;
         }
     }
+}
+
+const registerUser = async (_prevState: any, formData: FormData) => {
+    // CHECK: Valid use of 'as' in this
+    const username = formData.get('username') as string;
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+
+    const userDetails = { username, email, password };
+    const newState = await sendAuthRequest(userDetails, register);
+
+    return newState;
+}
+
+const loginUser = async (_prevState: any, formData: FormData) => {
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+
+    const userDetails = { email, password };
+    const newState = await sendAuthRequest(userDetails, login);
+
+    return newState;
 }
 
 export { registerUser, loginUser };
