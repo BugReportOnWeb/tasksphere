0d8d7c75991d549873feae98875768a401ff9172
Dev <flashytesting@gmail.com>
2023-12-11 21:45:12 +0530

    Add helper function 'sendReqeust' for auth server request

--- client/lib/user.ts
+++ client/lib/user.ts
@@ -1,50 +1,39 @@
 // TODO: Move to (types/) dir or something
 
-import { LoginUserDetails, RegisterUserDetails } from "@/types/auth";
+import { AuthUser, LoginUserDetails, RegisterUserDetails } from "@/types/auth";
 
 const BASE_URL = process.env.NODE_ENV === 'development'
     ? process.env.NEXT_PUBLIC_DEV_URL
     : process.env.NEXT_PUBLIC_PROD_URL
 
-// TOOD: Refactor for multiple errors and use when ready
-// Move to utils part (maybe?)
-// const sendRequest = async (
-//     userDetails: LoginUserDetails | RegisterUserDetails,
-//     slug: string
-// ) => {
-//     try {
-//         const res = await fetch(`${BASE_URL}/api/users/${slug}`, {
-//             method: 'POST',
-//             headers: { 'Content-Type': 'application/json' },
-//             body: JSON.stringify(userDetails),
-//             cache: 'no-store'
-//         })
-
-//         const data = await res.json();
-
-//         if (res.status === 400) {
-//             throw new Error(data.errors[0].msg);
-//         }
-
-//         if (!res.ok) throw new Error(data.error);
-
-//         return data;
-//     } catch (error) {
-//         throw error;
-//     }
-// }
-
-// TODO?: Maybe DRY in here... -> (sendRequest)
-const register = async (userDetails: RegisterUserDetails) => {
+// TODO: Move to utils part (maybe?)
+const sendRequest = async (
+    userDetails: LoginUserDetails | RegisterUserDetails,
+    slug: string
+): Promise<AuthUser> => {
     try {
-        const res = await fetch(`${BASE_URL}/api/users/register`, {
+        const res = await fetch(`${BASE_URL}/api/users/${slug}`, {
             method: 'POST',
-            headers: { "Content-Type": "application/json" },
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(userDetails),
             cache: 'no-store'
         })
 
         const data = await res.json();
+
+        // Possible reponse status code
+        // 400 -> Some validatin error in form (login/regsiter)
+        // 401 -> Wrong password (login)
+        // 404 -> User doesn't exist (login)
+        // 409 -> User already exist (register)
+        // 500 -> Some missed server error (server middleware)
+
+        // TODO: Do something for multiple errors
+        if (res.status === 400) {
+            console.error(data, res.status);
+            throw new Error(data.errors[0].msg);
+        }
+
         if (!res.ok) throw new Error(data.error);
 
         return data;
@@ -53,27 +42,19 @@ const register = async (userDetails: RegisterUserDetails) => {
     }
 }
 
+const register = async (userDetails: RegisterUserDetails) => {
+    try {
+        const user = await sendRequest(userDetails, 'register');
+        return user;
+    } catch (error) {
+        throw error;
+    }
+}
+
 const login = async (userDetails: LoginUserDetails) => {
     try {
-        const res = await fetch(`${BASE_URL}/api/users/login`, {
-            method: 'POST',
-            headers: { "Content-Type": 'application/json' },
-            body: JSON.stringify(userDetails),
-            cache: 'no-store'
-        })
-
-        const data = await res.json();
-
-        // Form validation failed
-        // CHECK: Some brute force method here it seemsj
-        // Refactor ASAP
-        if (res.status === 400) {
-            throw new Error(data.errors[0].msg);
-        }
-
-        if (!res.ok) throw new Error(data.error);
-
-        return data;
+        const user = await sendRequest(userDetails, 'login');
+        return user;
     } catch (error) {
         throw error;
     }
